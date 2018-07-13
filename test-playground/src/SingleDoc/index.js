fdadd890a2596f1c1afe941acb673364705496ca
sydeslyde <34600077+sydeslyde@users.noreply.github.com>
2018-07-13 15:12:52 +0200

    single document query already works with custom query

--- test-playground/src/SingleDoc/index.js
+++ test-playground/src/SingleDoc/index.js
@@ -4,30 +4,36 @@ import SingleDocForm from './Form';
 import pyrodux, { selectors, actions } from 'pyrodux';
 
 const singleDocQuery = pyrodux.firestore().collection("user").doc("lqkkwwdq4Gc4WMUUcKQa1GcWSI63");
-console.log(singleDocQuery);
 
 class SingleDocPage extends React.Component {
     componentDidMount() {
         this.props.loadData();
     }
 
+    componentWillUnmount() {
+        this.props.unloadData();
+    }
+
     render() {
+        console.log(this.props.initialValues);
         return <div>
             <p>
-                isLoading: {this.props.isLoading}
+                isLoading: {this.props.isLoading.toString()}
             </p>
-            <SingleDocForm initialValues={this.props.initialValues} onSubmit={this.props.handleSubmit} />
+            {!this.props.isLoading &&
+                <SingleDocForm initialValues={this.props.initialValues} onSubmit={this.props.handleSubmit} />
+            }
         </div>;
     }
 }
 
 export default connect(
     state => ({
-        isLoading: selectors.isLoading("singleDoc", state).toString(),
+        isLoading: selectors.isLoading("singleDoc", state),
         initialValues: selectors.asObject("singleDoc", state) // someSetting // otherSetting
     }),
     dispatch => ({
-        loadData: () => console.log("// TODO load data"),
+        loadData: () => dispatch(actions.data.retrieveQuery("singleDoc", singleDocQuery)),
         handleSubmit: (values) => console.log(values),
         unloadData: () => dispatch(actions.data.unloadCollectionOrQuery("singleDoc"))
     })
